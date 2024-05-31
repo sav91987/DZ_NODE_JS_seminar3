ad42b98921278583adb4574948454ef940922bb2
Sotnikov_Andrey <sav91987@ya.ru>
2024-05-31 15:18:46 +0300

    tryCatch added

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { error } = require("console");
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
@@ -5,29 +6,47 @@ const path = require("path");
 const app = express();
 const pathDir = path.join(__dirname, "public");
 
-fs.writeFileSync(path.join(pathDir, "countMain.txt"), "0");
-fs.writeFileSync(path.join(pathDir, "countAbout.txt"), "0");
+try {
+    fs.writeFileSync(path.join(pathDir, "countMain.txt"), "0");
+    fs.writeFileSync(path.join(pathDir, "countAbout.txt"), "0");
+} catch (error) {
+    console.error(error);
+}
 
-let countMain = Number(fs.readFileSync(path.join(pathDir, "countMain.txt")));
-let countAbout = Number(fs.readFileSync(path.join(pathDir, "countAbout.txt")));
-//const countPageNotFound = fs.readFileSync(path.join(pathDir, "countPageNotFound.txt"));
+let countMain = Number(
+    fs.readFileSync(path.join(pathDir, "countMain.txt"), "utf-8")
+);
+let countAbout = Number(
+    fs.readFileSync(path.join(pathDir, "countAbout.txt"), "utf-8")
+);
 
 app.get("/", (req, res) => {
-    fs.writeFileSync(path.join(pathDir, "countMain.txt"), `${++countMain}`);
-    res.send(`
-<h1>Корневая страница</h1>
-<h2>Просмотров на корневой странице ${countMain}</h2>
-<a href="/about">Перейти на страницу about</a>
-`);
+    try {
+        fs.writeFileSync(path.join(pathDir, "countMain.txt"), `${++countMain}`);
+        res.send(`
+    <h1>Корневая страница</h1>
+    <h2>Просмотров на корневой странице ${countMain}</h2>
+    <a href="/about">Перейти на страницу about</a>
+    `);
+    } catch (error) {
+        console.error(error);
+    }
 });
 
 app.get("/about", (req, res) => {
-    fs.writeFileSync(path.join(pathDir, "countAbout.txt"), `${++countAbout}`);
-    res.send(`
-<h1>About</h1>
-<h2>Просмотров на странице About ${countAbout}</h2>
-<a href="/">Перейти на корневую страницу</a>
-`);
+    try {
+        fs.writeFileSync(
+            path.join(pathDir, "countAbout.txt"),
+            `${++countAbout}`
+        );
+        res.send(`
+    <h1>About</h1>
+    <h2>Просмотров на странице About ${countAbout}</h2>
+    <a href="/">Перейти на корневую страницу</a>
+    `);
+    } catch (error) {
+        console.error(error);
+    }
 });
 
 app.use(express.static("public"));
